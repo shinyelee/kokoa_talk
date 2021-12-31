23161d01d32e11f2fe9bf73dcc477de729ac3b70
shinyelee <shinye0213@naver.com>
2021-12-31 10:56:22 +0900

    Update weather.js, weather icon

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,29 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
+const weather = document.querySelector("#weather .weather");
+const city = document.querySelector("#weather .city");
+const icon = document.querySelector("#weather .icon");
 const API_KEY = "8cd894e12de3fa5eaa1066e961a9fd37";
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}˚C`;
+      //
+      const icon = data.weather[0].icon;
+      imgIcon = `http://openweathermap.org/img/w/${icon}.png`;
+      const image = new Image();
+      image.src = imgIcon;
+      weather.appendChild(image);
+      //
     });
-  }
+}
+function onGeoError() {
+  alert("위치를 찾을 수 없습니다.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
-  function onGeoError() {
-    alert("Can't find you. No weather for you.");
-  }
-  
-  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+// https://namjackson.tistory.com/27

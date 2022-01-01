0e509d3c0566d442f88363b93346a54b85be5acb
shinyelee <shinye0213@naver.com>
2022-01-01 19:26:47 +0900

    Update saveToDos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -12,7 +18,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo;
   const button = document.createElement("button");
-  button.innerText = " ❌ ";
+  button.innerText = "❌ ";
   button.addEventListener("click", deleteToDo);
   li.appendChild(button);
   li.appendChild(span);
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
